2e8a4895e42ed70900f47c8034ce4b799da82875
Faree0 <exequiel_ali18@hotmail.com>
2023-06-14 21:27:13 -0300

    M03 Finalizado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,7 +5,7 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
-   if (x == y){
+   if (x === y){
       return true;
    } else {
       return false;
@@ -16,7 +16,7 @@ function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
-   if (str1.length == str2.length) {
+   if (str1.length === str2.length) {
       return true; 
    } else {
       return false;
@@ -49,7 +49,7 @@ function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
-   if (num % 2 == 0) {
+   if (num % 2 === 0) {
       return true;
    } else {
       return false;
@@ -60,7 +60,7 @@ function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
-   if (num % 2 != 0) {
+   if (num % 2 !== 0) {
       return true;
    } else {
       return false;
